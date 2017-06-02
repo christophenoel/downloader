5e61a543d82efe5b6f8d21e5e323cf03305698a2
mng <mng@h-pcmng.spb.spacebel.be>
2017-06-02 09:01:09 +0200

    - Correct configuration rest service

--- downloader-rest-service/src/main/java/esa/mep/downloader/service/Configuration.java
+++ downloader-rest-service/src/main/java/esa/mep/downloader/service/Configuration.java
@@ -18,7 +18,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,8 +34,7 @@ public class Configuration {
 
     @EJB
     private DownloaderLogic downloader;
-
-    @Context
+    
     private final static ObjectFactory of = new ObjectFactory();
 
     /**
