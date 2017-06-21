da520a2545bfda53629f4ec558c5b36bc2e03957
cnl <cnl@l-pccnl.spb.spacebel.be>
2017-06-21 10:44:40 +0200

    Added getContent operation (needed for validation) and updated SOAPUI project

--- downloader-rest-service/src/main/java/esa/mep/downloader/service/Download.java
+++ downloader-rest-service/src/main/java/esa/mep/downloader/service/Download.java
@@ -11,15 +11,20 @@ import _int.esa.proba_v_mep.schemas.downloader.DownloadStatus;
 import _int.esa.proba_v_mep.schemas.downloader.ObjectFactory;
 import esa.mep.downloader.logic.DownloaderException;
 import esa.mep.downloader.logic.DownloaderLogic;
+import java.io.File;
+
+import java.io.IOException;
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -76,4 +81,43 @@ public class Download {
     public DownloadStatus deleteDownload(@PathParam("identifier") String identifier) throws DownloaderException {
         return downloader.cancel(identifier);
     }    
+    
+     @POST
+    @Path("/content")
+     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public String getContent(String filePath) throws DownloaderException {
+      return getTextFileContent(filePath);
+    }
+    
+      public String getTextFileContent(String filePath) {
+       
+        String content = null;
+        try {
+            File file = new File(filePath);
+            if (file.exists()) {
+                //System.out.println("MD5 Hex: " + DigestUtils.md5Hex(new FileInputStream(file)));
+                if (file.isFile()) {
+                    if (filePath.endsWith(".txt") || filePath.endsWith(".xml")) {
+                        content = FileUtils.readFileToString(file);
+                    } else {
+                        content = "" + FileUtils.sizeOf(file);
+                    }
+                } else {
+                    if (file.isDirectory()) {
+                        content = "" + FileUtils.sizeOfDirectory(file);
+                    } else {
+                        content = "NOR_FILE_NOR_DIRECTORY";
+                    }
+                }
+            } else {
+                content = "FILE_NOT_FOUND";
+            }
+        } catch (IOException e) {
+        
+        }
+        
+        return content;
+    }
 }
+
+
